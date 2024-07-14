e57950c9a8a45986df5e4414c2813683bfdac7be
Muammer Uslu <108658435+MuammerUsluImaginite@users.noreply.github.com>
2024-07-14 17:20:11 +0300

    Add mongo-db connection with .env file configuration

--- src/main/java/dev/uslu/movies/MoviesApplication.java
+++ src/main/java/dev/uslu/movies/MoviesApplication.java
@@ -1,7 +1,12 @@
 package dev.uslu.movies;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -10,4 +15,17 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Component
+	public class MongoConfigLogger {
+
+		private static final Logger logger = LoggerFactory.getLogger(MongoConfigLogger.class);
+
+		@Value("${spring.data.mongodb.uri}")
+		private String mongoUri;
+
+		@PostConstruct
+		public void logMongoUri() {
+			logger.info("MongoDB URI: {}", mongoUri);
+		}
+	}
 }
